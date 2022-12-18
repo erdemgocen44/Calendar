663fc09996c0bca29c98320e3e06d47e1b3279c7
Erdem <erdemgocen44@gmail.com>
2022-12-18 20:43:42 +0100

    habitTile commence

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,9 +11,10 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.green[300],
       body: ListView(
         children: [
-          HabitTile(),
-          HabitTile(),
-          HabitTile(),
+          HabitTile(
+            habitComplete: false,
+            habitName: 'Bonjour!!!',
+          ),
         ],
       ),
     );
