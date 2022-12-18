05169b3279f68e4e3e9499a39fb81de0dd019521
Erdem <erdemgocen44@gmail.com>
2022-12-18 19:15:25 +0100

    colour finir

--- lib/head_map_calendar.dart
+++ lib/head_map_calendar.dart
@@ -8,19 +8,18 @@ class MoiHeadMap extends StatelessWidget {
   Widget build(BuildContext context) {
     return HeatMap(
       datasets: {
-        DateTime(2022, 10, 23): 3,
-        DateTime(2022, 10, 24): 7,
-        DateTime(2022, 10, 25): 10,
-        DateTime(2022, 10, 26): 13,
-        DateTime(2022, 10, 27): 6,
+        DateTime(2022, 12, 23): 3,
+        DateTime(2022, 12, 24): 7,
+        DateTime(2022, 12, 25): 10,
+        DateTime(2022, 12, 26): 13,
+        DateTime(2022, 12, 27): 6,
       },
       startDate: DateTime.now(),
-      endDate: DateTime.now().add(
-        Duration(days: 40),
-      ),
+      endDate: DateTime.now().add(Duration(days: 40)),
       colorMode: ColorMode.opacity,
       showText: false,
       size: 35,
+      textColor: Colors.blueGrey,
       scrollable: true,
       colorsets: const {
         1: Color.fromARGB(120, 238, 216, 18),
@@ -30,9 +29,9 @@ class MoiHeadMap extends StatelessWidget {
         5: Color.fromARGB(200, 238, 216, 18),
         6: Color.fromARGB(220, 238, 216, 18),
         7: Color.fromARGB(240, 238, 216, 18),
-        8: Color.fromARGB(260, 238, 216, 18),
-        9: Color.fromARGB(280, 238, 216, 18),
-        10: Color.fromARGB(300, 238, 216, 18),
+        8: Color.fromARGB(250, 238, 216, 18),
+        9: Color.fromARGB(260, 238, 216, 18),
+        10: Color.fromARGB(270, 238, 216, 18),
       },
     );
   }
