a13e5646a520594da992d5f3a739c64046c7eaa1
Erdem <erdemgocen44@gmail.com>
2022-12-19 23:33:17 +0100

    habitTile commence

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           HabitTile(
             habitComplete: habitCompleted,
-            habitName: 'Bonjour!!!',
+            habitName: 'Bonjour!',
             onChanged: (value) => checkBoxTapped(value),
           ),
         ],
