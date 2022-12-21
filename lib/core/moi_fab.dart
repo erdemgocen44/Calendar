fdc342888a78d5a6e4f34fe0aed4a86c7dad75e4
Erdem <erdemgocen44@gmail.com>
2022-12-21 19:08:55 +0100

    created floatingactionbutton

--- lib/core/moi_fab.dart
+++ lib/core/moi_fab.dart
@@ -5,6 +5,8 @@ class MyFloatingActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FloatingActionButton(
+      onPressed: () {},
+    );
   }
 }
