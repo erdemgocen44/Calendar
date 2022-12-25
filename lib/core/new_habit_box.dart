366b9c7a34f6a949c2a4fba6e24731f4a6b82b0f
Erdem <erdemgocen44@gmail.com>
2022-12-25 23:33:02 +0100

    created enternewdialogbox

---
+++ lib/core/new_habit_box.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EnterNewHabitBox extends StatelessWidget {
+  const EnterNewHabitBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
