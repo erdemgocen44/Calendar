c9780743357d673eb1488aec4a4890172d1b56b7
Erdem <erdemgocen44@gmail.com>
2023-01-02 18:29:29 +0100

    slidable çalışıyor

--- lib/core/habit_tile.dart
+++ lib/core/habit_tile.dart
@@ -4,6 +4,9 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 class HabitTile extends StatelessWidget {
   final String habitName;
   final bool habitComplete;
+  final Function(BuildContext)? settingTapped;
+  final Function(BuildContext)? deleteTapped;
+
   final Function(bool?)? onChanged;
   const HabitTile(
       {Key? key,
@@ -31,12 +34,12 @@ class HabitTile extends StatelessWidget {
 
             //Delete option
 
-              SlidableAction(
+            SlidableAction(
               borderRadius: BorderRadius.circular(15),
               //Bu widget ile kaydırarak maillerde olduğu gibi işlem yaparız par exemple: silme, arşivleme, başka dosyaya taşıma
               onPressed: (context) {},
               backgroundColor: Colors.redAccent,
-              icon: Icons.settings,
+              icon: Icons.delete,
             ),
           ],
         ),
